91dbc685b59e26386c329b943b5f39cbac0c7e79
Sid Baldwin <sid@ftg.co.uk>
2014-03-31 20:43:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pi Alarm
+
+Monitoring an ADE Honeywell Accenta Mini Gen4 alarm system using the Remote Signalling Input and Outputs (although I have yet to find an input...). 
+
+The Accenta uses 4 Remote Signalling outputs for use with eg a speech dialer. These are connected to the Raspberry Pi's GPIO pins via Schmitt trigger isolators.
+
+## Circuit diagram:
+
+![Circuit Diagram](Accenta2Pi.png)
+
+
---
+++ alarm.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+from sitewinder.utils import sms, prowl
+
+# Signal: 'INT','PA','SET','ABORT'
+# GPIO: 3, 5, 7, 11
+
+switches = [3, 5, 7, 11]
+states = [True, True, True, True]
+
+def _setup():
+  GPIO.setwarnings(False) 
+  GPIO.setmode(GPIO.BOARD)
+  for switch in switches:
+    GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
+
+if __name__ == "__main__":
+  _setup()
+  while 1:
+    for i in range(0, len(switches)):
+      valSwitch = GPIO.input(switches[i])
+      valState = states[i]
+      if (valSwitch != valState):
+        nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+        if i==0 and valSwitch==False:
+          msg='ALARM!'
+        elif i==1 and valSwitch==False:
+          msg='PANIC!'
+        elif i==2 and valSwitch==True:
+          msg='Alarm unset'
+        elif i==2 and valSwitch==False:
+          msg='Alarm set'
+        elif i==3 and valSwitch==True:
+          msg='System restored'
+        elif i==3 and valSwitch==False:
+          msg='Awaiting restore'
+        else:
+          msg=''
+        if msg:
+          # Always log the new state
+          with open('/home/pi/inetpub/alarm.txt', 'a') as myfile:
+            myfile.write(nowTime  + ': ' + msg + '\n')
+          # Always prowl the new state
+          prowl(msg)
+          # SMS if Alarm or Duress
+          if msg in ("ALARM!", "PANIC!"):
+            sms(msg)
+          # Remember the new states
+          states[i] = valSwitch
+    time.sleep(0.1)
