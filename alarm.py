00da00991919efe269c98e03981c505611f45b6c
Sid Baldwin <sid@ftg.co.uk>
2014-09-25 20:20:59 +0100

    Refactor
    Simplified logic

--- alarm.py
+++ alarm.py
@@ -2,13 +2,11 @@
 
 import RPi.GPIO as GPIO
 import time
-from sitewinder.utils import sms, prowl
 
 # Signal: 'INT','PA','SET','ABORT'
-# GPIO: 3, 5, 7, 11
-
-switches = [3, 5, 7, 11]
-states = [True, True, True, True]
+switches = [22, 11, 13, 15]
+states = [0, 0, 0, 0]
+oldMsg = ''
 
 def _setup():
   GPIO.setwarnings(False) 
@@ -20,33 +18,19 @@ if __name__ == "__main__":
   _setup()
   while 1:
     for i in range(0, len(switches)):
-      valSwitch = GPIO.input(switches[i])
-      valState = states[i]
-      if (valSwitch != valState):
-        nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
-        if i==0 and valSwitch==False:
-          msg='ALARM!'
-        elif i==1 and valSwitch==False:
-          msg='PANIC!'
-        elif i==2 and valSwitch==True:
-          msg='Alarm unset'
-        elif i==2 and valSwitch==False:
-          msg='Alarm set'
-        elif i==3 and valSwitch==True:
-          msg='System restored'
-        elif i==3 and valSwitch==False:
-          msg='Awaiting restore'
-        else:
-          msg=''
-        if msg:
-          # Always log the new state
-          with open('/home/pi/inetpub/alarm.txt', 'a') as myfile:
-            myfile.write(nowTime  + ': ' + msg + '\n')
-          # Always prowl the new state
-          prowl(msg, msg)
-          # SMS if Alarm or Duress
-          if msg in ("ALARM!", "PANIC!"):
-            sms(msg)
-          # Remember the new states
-          states[i] = valSwitch
+      states[i] = GPIO.input(switches[i])
+    if states[0] == 0:
+      newMsg = 'ALARM!'
+    elif states[1] == 0:
+      newMsg = 'PANIC!'
+    elif states[2] == 0:
+      newMsg = 'Alarm set'
+    elif states[3] == 0:
+      newMsg = 'Awaiting restore'
+    else:
+      newMsg = 'Alarm unset'
+    if newMsg <> oldMsg:
+      # Do something here eg email/sms/growl...!
+      print newMsg
+    oldMsg = newMsg
     time.sleep(0.1)
